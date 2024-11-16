89d0044ac3978c22b41e6cd37e26b9d25d71db3d
Verner Fortelius <verner@fortelius.net>
2024-11-16 17:11:18 +0200

    publish aot

--- NerAnonymizerConsoleTester/Program.cs
+++ NerAnonymizerConsoleTester/Program.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.ML.OnnxRuntime;
 using NerAnonymizer;
 using vforteli.WordPieceTokenizer;
@@ -9,10 +10,14 @@ const string vocabPath = "../../../../finbert-ner-onnx/vocab.txt";
 const string modelPath = "../../../../finbert-ner-onnx/model.onnx";
 const string configPath = "../../../../finbert-ner-onnx/config.json";
 
-var jsonSerializerOptions = new JsonSerializerOptions { WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping };
+var jsonSerializerOptions = new JsonSerializerOptions
+    { WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping };
 
 var vocabulary = await File.ReadAllTextAsync(vocabPath);
-var config = await JsonSerializer.DeserializeAsync<BertNerModelConfig>(File.OpenRead(configPath)) ?? throw new ArgumentNullException("config");
+var config =
+    await JsonSerializer.DeserializeAsync<BertNerModelConfig>(File.OpenRead(configPath),
+        SourceGenerationContext.Default.BertNerModelConfig) ??
+    throw new ArgumentNullException("config");
 var tokenizer = new Lazy<WordPieceTokenizer>(() => new WordPieceTokenizer(vocabulary));
 var inferenceSession = new Lazy<InferenceSession>(() => new InferenceSession(modelPath));
 
@@ -33,7 +38,7 @@ for (var i = 0; i < 300; i++)
     Console.WriteLine("Sup " + i);
     var groupedResults = runner.RunClassification(input).ToList();
     Console.WriteLine(GC.GetTotalAllocatedBytes() - allocations);
-    
+
     // Console.WriteLine(Utils.Anonymize(input, groupedResults));
 }
 
@@ -45,3 +50,9 @@ Console.WriteLine($"Done after {stopwatch.ElapsedMilliseconds}ms");
 // Console.WriteLine(Utils.Anonymize(input, groupedResults));
 
 // 16822552
+
+
+[JsonSerializable(typeof(BertNerModelConfig))]
+internal partial class SourceGenerationContext : JsonSerializerContext
+{
+}
