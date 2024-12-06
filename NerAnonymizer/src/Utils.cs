5f59f6a46711630e14ac531edcf79caaacbcfdc7
Verner Fortelius <verner@fortelius.net>
2024-12-06 12:30:47 +0200

    prediction trimming
    util for trimming overlapping predictions

--- NerAnonymizer/src/Utils.cs
+++ NerAnonymizer/src/Utils.cs
@@ -18,4 +18,39 @@ public class Utils
 
         return sb.ToString();
     }
+
+    /// <summary>
+    /// Group overlapping predictions and return best*.
+    /// * best here is quite undefined
+    /// </summary>    
+    public static IEnumerable<PredictionResult> TrimOverlapping(IEnumerable<PredictionResult> predictions)
+    {
+        using var predictionsEnumerator = predictions.GetEnumerator();
+
+        if (!predictionsEnumerator.MoveNext()) yield break;
+
+        var bestPrediction = predictionsEnumerator.Current;
+        var predictionEndIndex = predictionsEnumerator.Current.End;
+
+        while (predictionsEnumerator.MoveNext())
+        {
+            var match = predictionsEnumerator.Current;
+
+            if (predictionsEnumerator.Current.Start > predictionEndIndex)
+            {
+                yield return bestPrediction;
+
+                bestPrediction = predictionsEnumerator.Current;
+            }
+
+            if (match.Score > bestPrediction.Score)
+            {
+                bestPrediction = match;
+            }
+
+            predictionEndIndex = predictionsEnumerator.Current.Start;
+        }
+
+        yield return bestPrediction;
+    }
 }
